e7c79b257aec37bd4c18a4aa2c9e236e44164abc
ItsVibhanshuSharma <vibhanshusharma1811@gmail.com>
2023-09-27 23:26:56 +0530

    Third commit

--- src/screen/login.js
+++ src/screen/login.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-
+import {Button, Text, View} from 'react-native';
+import Animated, {useSharedValue, withSpring} from 'react-native-reanimated';
 export default function login() {
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
   return (
     <View>
       <Text>Login </Text>
+
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
     </View>
   );
 }
