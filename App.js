5b97c8a4229b7a48e28c306a9cc65419f7400729
ItsVibhanshuSharma <vibhanshusharma1811@gmail.com>
2023-09-27 23:03:18 +0530

    Second commit

---
+++ App.js
@@ -0,0 +1,21 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import login from './src/screen/login';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
